c5a32b74a65695d63c5c4b03c92a432cdcecbc34
Marina Coelho <marinacoelho16@gmail.com>
2023-05-12 11:40:30 +0100

    Updated gradle files to kts syntax (#5)
    * Migrated gradle files in start folder
    
    * Migrated gradle files in final folder

---
+++ start/build.gradle.kts
@@ -0,0 +1,23 @@
+import java.net.URI
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenLocal()
+        mavenCentral()
+        maven { url = URI("https://jitpack.io") }
+    }
+}
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
